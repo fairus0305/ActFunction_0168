a6dc8cf11dd0eadaebccd9e18cd9ad23a35261c8
fairus0305 <ruszezzy@gmail.com>
2026-03-02 15:27:36 +0700

    menambah komen prosedur

--- luasLingkaran.cpp
+++ luasLingkaran.cpp
@@ -3,19 +3,24 @@
 using namespace std; 
 
 //deklarasi variabel
-float r;
+int r;
 
+//prosedur
 void input(){
     cout << "masukan r :";
     cin >> r;
 }
 
-float LuasLingkaran(float a){ 
-    return a * a * 3.14159 ;
+
+float LuasLingkaran(float a){
+    return 3.14159 * a * a;
 }
 
 void output(){
     cout << "hasil  : " << LuasLingkaran(r);
 }
 
-
+int main(){
+    input();
+    output();
+}
